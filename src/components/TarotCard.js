dc3109a7553e511972f2f3313178839fee70d2b9
levipols <levipols@gmail.com>
2023-04-15 17:38:42 +0200

    removed flipping

--- src/components/TarotCard.js
+++ src/components/TarotCard.js
@@ -1,20 +1,15 @@
 // src/components/TarotCard.js
 
-import React, { useState } from 'react';
+import React from 'react';
 
 const TarotCard = ({ card }) => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
   return (
-    <div
-      className={`tarot-card${isFlipped ? ' tarot-card-flipped' : ''}`}
-      onClick={() => setIsFlipped(!isFlipped)}
-    >
-      <div className="tarot-card-image-container">
-        <img src={card.image} alt={card.name} className="tarot-card-image" />
-      </div>
-      <div className="tarot-card-name">{card.name}</div>
+    <div className="tarot-card">
+    <div className="tarot-card-image-container">
+      <img src={card.image} alt={card.name} className="tarot-card-image" />
     </div>
+    <div className="tarot-card-name">{card.name}</div>
+  </div>
   );
 };
 
