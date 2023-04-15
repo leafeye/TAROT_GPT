bdc825e3f47912f651c848e75bc6f8f6ce801c9c
levipols <levipols@gmail.com>
2023-04-15 18:03:02 +0200

    added glitch effect

--- src/components/TarotCard.js
+++ src/components/TarotCard.js
@@ -7,6 +7,10 @@ const TarotCard = ({ card }) => {
     <div className="tarot-card">
     <div className="tarot-card-image-container">
       <img src={card.image} alt={card.name} className="tarot-card-image" />
+      <div className="tarot-card-image-glitch" style={{ backgroundImage: `url(${card.image})` }}></div>
+      <div className="tarot-card-image-glitch" style={{ backgroundImage: `url(${card.image})` }}></div>
+      <div className="tarot-card-image-glitch" style={{ backgroundImage: `url(${card.image})` }}></div>
+
     </div>
     <div className="tarot-card-name">{card.name}</div>
   </div>
